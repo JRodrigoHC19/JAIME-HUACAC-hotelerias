605ebe200e4f99df0ed2e77c7cff90874c003c3c
RoderickJ <114635654+JRodrigoHC19@users.noreply.github.com>
2023-05-15 16:50:17 -0500

    Update code_editar.php

--- code_editar.php
+++ code_editar.php
@@ -17,8 +17,8 @@
     $resultado = $sentencia->execute([$titulo, $categoria, $ubi_1, $ubi_2, $ubi_3, $date_ins, $telf, $state, $codigo]);
 
     if ($resultado == TRUE) {
-        header('Location: https://jaime-huacac-hotelerias-production.up.railway.app/index.php?mensaje=editado');
+        header('Location: index.php?mensaje=editado');
     } else {
-        header('Location: https://jaime-huacac-hotelerias-production.up.railway.app/index.php?mensaje=error');
+        header('Location: index.php?mensaje=error');
         exit();
     }
