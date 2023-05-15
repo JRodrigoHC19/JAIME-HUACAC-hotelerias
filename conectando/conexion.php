db2eae362afb2c25d04e061704489f7e921f89e3
RoderickJ <114635654+JRodrigoHC19@users.noreply.github.com>
2023-05-15 16:05:22 -0500

    Update conexion.php

--- conectando/conexion.php
+++ conectando/conexion.php
@@ -10,10 +10,10 @@ try {
     // $database = mysqli_connect ("containers-us-west-180.railway.app",$user,$pwd,$db_name,$port);
     // $database->exec("SET CHARACTER SET utf8");
 
-    // $database = new PDO ('mysql:host=containers-us-west-74.railway.app;por=7433;dbname='.$db_name, $user,$pwd,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+    $database = new PDO ('mysql:host=containers-us-west-201.railway.app;port=5664;dbname='.$db_name, $user,$pwd,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 
     // $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $database = new PDO ("mysql://root:zOAssqYyoHpzomsCulUD@containers-us-west-201.railway.app:5664/railway");
+    // $database = new PDO ("mysql://root:zOAssqYyoHpzomsCulUD@containers-us-west-201.railway.app:5664/railway");
     // $database = new PDO ($URL);
     // echo "Connected Successfully";
 } catch (Exception $e) {
